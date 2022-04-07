f851ff640a72ea2e308145af9fd73f6f73814e4a
Willian Alves Fonseca <willian.alves2109@gmail.com>
2022-04-07 16:59:35 -0300

    Finalizando AULA 2 - CURSO 2  - Referência Ponteiro e Método

--- curso-02-orientado-a-objeto/src/main.go
+++ curso-02-orientado-a-objeto/src/main.go
@@ -10,10 +10,33 @@ type contaCorrente struct {
 	saldo   float64
 }
 
-func main() {
+// Função para sacar
+func (c *contaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque <= c.saldo
 
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saldo realizado com sucesso!"
+	} else {
+		return "saldo insuficiente"
+	}
+}
+
+func main() {
 	// Forma mais usual de utilizar o struct
-	contaGuilherme := contaCorrente{"Willian", 296, 32, 4988.30}
+	contaGuilherme := contaCorrente{"Willian Fonseca", 296, 32, 4988.30}
 	fmt.Println(contaGuilherme)
 
+	// Outra forma de instanciar e usar o instruct.
+	// Nesta forma o * é usado como direcionamento, um ponteiro para a referencia da variavel.
+	var contaDaCris *contaCorrente
+	contaDaCris = new(contaCorrente)
+	contaDaCris.titular = "Cris Oliveira"
+	contaDaCris.agencia = 326
+	contaDaCris.conta = 3265045
+	contaDaCris.saldo = 6000.32
+	fmt.Println(contaDaCris)
+
+	fmt.Println(contaDaCris.Sacar(1200.00))
+	fmt.Println(contaDaCris)
 }
